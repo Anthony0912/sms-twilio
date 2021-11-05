341de4016635221ef986e00e5fb4bbf66572a686
akcardona <keylor@jorshcastle.com>
2021-11-04 21:36:22 -0600

    Configuracion de sms con twilio

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import { Twilio } from "twilio";
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioNumber = process.env.TWILIO_PHONE_NUMBER;
+const myNumber = process.env.MY_NUMBER;
+
+const app = express();
+const port = 3000;
+
+if (accountSid && authToken && myNumber && twilioNumber) {
+const client = new Twilio(accountSid, authToken);
+
+client.messages
+    .create({
+      from: twilioNumber,
+      to: myNumber,
+      body: "You just sent an SMS from TypeScript using Twilio!",
+    })
+    .then((message) => console.log(message.sid));
+} else {
+    console.error(
+      "You are missing one of the variables you need to send a message"
+    );
+  }
+
+app.listen(port, () => {
+  console.log(`Timezones by location application is running on port ${port}.`);
+});
